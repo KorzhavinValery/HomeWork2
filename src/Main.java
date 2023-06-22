07149d265646ee9c102c44604415077e1259cc1d
Vel <korzhavin.vel@yandex.ru>
2023-06-22 18:18:38 +0700

    Homework2 Task1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        var dog = 8.0; //переменной dog значение 8.0;
+            System.out.println(dog);
+
+        var cat = 3.6;// переменной cat значение 3.6;
+            System.out.println(cat);
+
+        var paper = 763789;// переменной paper значение 763789.
+            System.out.println(paper);
 
     }
 }
