354e739c6b7aff619ea3419256f011d3397a89c3
Vel <korzhavin.vel@yandex.ru>
2023-06-22 18:21:28 +0700

    Homework2 Task2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8.0; //переменной dog значение 8.0;
             System.out.println(dog);
-
+            dog = dog + 4;
+            System.out.println(dog);
         var cat = 3.6;// переменной cat значение 3.6;
             System.out.println(cat);
-
+            cat = cat + 4;
+            System.out.println(cat);
         var paper = 763789;// переменной paper значение 763789.
             System.out.println(paper);
-
+            paper = paper + 4;
+            System.out.println(paper);
     }
 }
